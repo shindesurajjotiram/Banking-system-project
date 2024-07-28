0f2dee6dd297d2020295c44e9a8c2c45c950e979
shindesurajjotiram <suraj.jotiram@godigit.com>
2024-07-28 13:02:30 +0530

    the Account abstract class is created for project

---
+++ Account.java
@@ -0,0 +1,36 @@
+package bankingsystem;
+
+public abstract class Account {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public Account(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        } else {
+            throw new IllegalArgumentException("Balance cannot be negative");
+        }
+    }
+
+    public abstract double calculateInterest();
+    public abstract void displayAccountDetails();
+}
